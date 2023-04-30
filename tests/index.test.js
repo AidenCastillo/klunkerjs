c76aa564b44acdf6f15c4d8fbcb73906843cf858
AidenCastillo <amc-mcjarjar@hotmail.com>
2023-04-30 14:49:00 -0500

    logger test

--- tests/index.test.js
+++ tests/index.test.js
@@ -6,8 +6,15 @@ test('check database discordusers connection', async () => {
 			const discordusers = await pb.collection("discordusers").getFullList(200, {
 				sort : "-created"
 			});
-			console.log(discordusers);
+			// console.log(discordusers);
 		}
 		const data = await getDiscordUsers();
 		expect(data).toBeDefined;
+})
+
+test('check discord Client', async () => {
+		const { Client, Collection, Events, GatewayIntentBits } = require("discord.js");
+		const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+		// console.log(client);
+		expect(client).toBeDefined;
 })
